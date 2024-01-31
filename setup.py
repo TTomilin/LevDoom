a434004eb02f200ff539e34319e638fc03a6242b
TTomilin <tristan.tomilin@hotmail.com>
2024-01-31 11:46:41 +0100

    Added classifiers

--- setup.py
+++ setup.py
@@ -26,4 +26,9 @@ setup(
     include_package_data=True,
     install_requires=levdoom_requirements,
     extras_require={'rl': rl_requirements},
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
 )
